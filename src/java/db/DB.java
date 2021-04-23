7a853120d165f9822094b5994a57e171a8611044
tnicacio <nicacio.t92@gmail.com>
2021-04-22 22:10:09 -0300

    creating database connection class

---
+++ src/java/db/DB.java
@@ -0,0 +1,58 @@
+package db;
+
+import java.sql.*;
+
+/**
+ *
+ * @author tnica
+ */
+public class DB {
+    
+     private static Connection conn = null;
+    
+    public static Connection getConnection(){
+        String url = "jdbc:postgresql://localhost:5432/pg5trab1";
+        String user = "postgres";
+        String password = "123456";
+        
+        if (conn == null){
+            try {
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection(url, user, password);
+            } catch (SQLException | ClassNotFoundException e) {
+                 System.out.println(e.getMessage());
+            }   
+        }
+        return conn;
+    }
+    
+    public static void closeConnection(){
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+        public static void closeStatement(Statement sttmt){
+        if (sttmt != null){
+            try {
+                sttmt.close();
+            } catch (SQLException e) {
+                 System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    public static void closeResultSet(ResultSet rs){
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                 System.out.println(e.getMessage());
+            }
+        }
+    }
+}
