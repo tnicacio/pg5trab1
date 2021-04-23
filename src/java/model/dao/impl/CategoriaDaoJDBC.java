a5b03d1e05d4d64b383e6bb499d45c23ac6601ed
tnicacio <nicacio.t92@gmail.com>
2021-04-22 22:20:25 -0300

    Categoria Insert implementation

--- src/java/model/dao/impl/CategoriaDaoJDBC.java
+++ src/java/model/dao/impl/CategoriaDaoJDBC.java
@@ -1,5 +1,12 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import model.dao.CategoriaDAO;
 import model.entities.Categoria;
@@ -9,10 +16,41 @@ import model.entities.Categoria;
  * @author tnica
  */
 public class CategoriaDaoJDBC implements CategoriaDAO {
+    
+    private Connection conn;
+    
+    public CategoriaDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Categoria obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            String sql = "INSERT INTO categoria(descricao) " +
+                    "values(?)";
+            st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, obj.getDescricao());
+            
+            int rowsAffected = st.executeUpdate();
+            
+            if (rowsAffected > 0) {
+                rs = st.getGeneratedKeys();
+                if (rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setIdcategoria(id);
+                }
+            } else {
+                throw new DbException("Unexpected error! No rows were affected!");  
+            }
+            
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+        } 
     }
 
     @Override
