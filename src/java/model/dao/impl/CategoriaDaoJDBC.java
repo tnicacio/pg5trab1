32be5cef22c9c049f13beb569a3fad0c268afb04
tnicacio <nicacio.t92@gmail.com>
2021-04-22 22:16:10 -0300

    creating Categoria DAO JDBC class

---
+++ src/java/model/dao/impl/CategoriaDaoJDBC.java
@@ -0,0 +1,43 @@
+package model.dao.impl;
+
+import java.util.List;
+import model.dao.CategoriaDAO;
+import model.entities.Categoria;
+
+/**
+ *
+ * @author tnica
+ */
+public class CategoriaDaoJDBC implements CategoriaDAO {
+
+    @Override
+    public void insert(Categoria obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Categoria obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Categoria findById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Categoria> findByName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Categoria> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
